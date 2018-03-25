9a8db7599f14f1916491306a7a03a30e068c8b95
insighthorizon <majiricek@seznam.cz>
2018-03-25 17:00:32 +0200

    Update .emacs.el

--- .emacs.el
+++ .emacs.el
@@ -1,3 +1,9 @@
+;; Custom init file for EMACS
+
+;; ename line numbering
+;; truncat elines
+;; set collor theme 
+
 (global-linum-mode 1)
 (set-default 'truncate-lines t)
 (load-theme 'tango-dark t)
