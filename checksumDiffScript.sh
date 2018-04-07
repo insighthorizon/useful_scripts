c576914a35c5f8b822411592c6fe7365e1c96c15
insighthorizon <majiricek@seznam.cz>
2018-04-07 13:53:02 +0200

    "reuploading merged file .emacs.el and updating others"

--- checksumDiffScript.sh
+++ checksumDiffScript.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 #using diff for comparing two "files", using < to put echo output there instead of files, using | - pipeline to fill echo with sha256sum or md5sum output
 
 diff  <(echo "da9510fd2a1a213db7a2db7f3db81086c8d6699078bbdbd940a7b228a18f3624  Archlinux_2017.08_KDE-VB-64bit.7z" ) <(echo | sha256sum Archlinux_2017.08_KDE-VB-64bit.7z)
--- disassemble.sh
+++ disassemble.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+#creating assembly file out of exe file
+
 (echo | objdump -d install.exe) > disassembly1.s
 
