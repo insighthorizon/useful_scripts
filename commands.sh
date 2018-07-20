bb2ebb351db518dc0108d391b6e185ea491032da
insighthorizon <majiricek@seznam.cz>
2018-07-20 09:17:53 +0200

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,3 +1,7 @@
 #Open emacs in new terminal window
 gnome-terminal -e "emacs -nw main.cpp"
 x-terminal-emulator -e "emacs -nw"
+
+#Create static library
+gcc -c libMystaticLib.c
+ar rcs libMyStaticLib.a libMyStaticLib.o 
