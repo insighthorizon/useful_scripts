aac5ffb4d53a2920681627451ac9ba7e05d4934b
insighthorizon <majiricek@seznam.cz>
2018-07-31 23:55:03 +0200

    Update index_tags.sh

--- index_tags.sh
+++ index_tags.sh
@@ -2,5 +2,7 @@
 
 #create tags for emaxs - enables jumping to definitions etc
 
+#Then, while editing your source code, when you want to jump to a declaration, press M-. 
+
 rm -f TAGS
 find . -name "*.[ch]" -print | xargs etags -a
