22f9b89613a570ca678881254adb62114b0147fb
insighthorizon <majiricek@seznam.cz>
2018-08-06 13:07:14 +0200

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -5,3 +5,16 @@ x-terminal-emulator -e "emacs -nw"
 #Create static library
 gcc -c libMystaticLib.c
 ar rcs libMyStaticLib.a libMyStaticLib.o 
+
+#How to run programs in terminal background
+./add-server & # starts running dbus_test program in background
+#same can be done with
+./add-server
+ctrl - z
+bg
+# to kill the process:
+fg
+ctrl - c
+#display background processes
+jobs
+
