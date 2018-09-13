b32283e70b7990cc801ab54ad5c573976c100eaf
insighthorizon <majiricek@seznam.cz>
2018-09-13 14:37:28 +0200

    Update .emacs.el

--- .emacs.el
+++ .emacs.el
@@ -1,12 +1,9 @@
+;; Custom init file for EMACS
+
 ;; Linux location: /home/user/.emacs.el
 ;; Windows 10 location: drive:\Users\user\AppData\Roaming\.emacs
 
-;; Custom init file for EMACS
-
-;; ename line numbering
-;; truncat elines
-;; set collor theme
-
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(setq inhibit-startup-screen t)
 (global-linum-mode 1)
-(set-default 'truncate-lines t)
-(load-theme 'tango-dark t)
