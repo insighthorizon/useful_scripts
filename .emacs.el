9d510ae120662ab992204ff3b1904fe1deec0995
insighthorizon <majiricek@seznam.cz>
2018-09-22 12:08:00 +0200

    Update .emacs.el

--- .emacs.el
+++ .emacs.el
@@ -1,27 +1,25 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
-(setq inhibit-startup-screen t)
+(load-file "~/.emacs.d/themes/heroku-theme.el")
+(load-theme 'heroku t)
 (global-linum-mode 1)
 
 (require 'org)
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(custom-enabled-themes (quote (whiteboard))))
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(term-color-blue ((t (:background "deep sky blue" :foreground "cyan"))))
+ '(term-color-red ((t (:background "salmon" :foreground "light coral")))))
+
 
