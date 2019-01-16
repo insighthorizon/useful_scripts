9cbc3ca3ba1bb10cabde96c689b8117ff4ba8039
insighthorizon <majiricek@seznam.cz>
2019-01-16 17:39:07 +0100

    Update .emacs.el

--- .emacs.el
+++ .emacs.el
@@ -4,10 +4,18 @@
 (load-theme 'heroku t)
 (global-linum-mode 1)
 
+(require 'package)
+(setq package-archives
+      '(("melpa" . "https://melpa.org/packages/")
+        ("gnu" . "https://elpa.gnu.org/packages/")
+        ("org" . "http://orgmode.org/elpa/")))
+(package-initialize)
+
 (require 'org)
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -22,4 +30,8 @@
  '(term-color-blue ((t (:background "deep sky blue" :foreground "cyan"))))
  '(term-color-red ((t (:background "salmon" :foreground "light coral")))))
 
+;;org mode latex preview size
+(setq org-format-latex-options (plist-put org-format-latex-options :scale 2.0))
 
+;; enable latex preview pan
+(load "preview-latex.el" nil t t)
